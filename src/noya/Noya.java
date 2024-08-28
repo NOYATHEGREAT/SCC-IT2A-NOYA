7c980ba030dd365f6d344ea998de02f128adbe74
NOYATHEGREAT <johnlloydisguapo@gmail.com>
2024-08-28 11:28:18 +0800

    Update Noya.java

--- src/noya/Noya.java
+++ src/noya/Noya.java
@@ -1,4 +1,3 @@
-
 package noya;
 
 import java.util.Scanner;
@@ -6,8 +5,19 @@ public class Noya {
 
     public static void main(String[] args) {
         
-        Grades grd = new Grades();
+        /* Grades grd = new Grades();
         grd.getGrades();
+        */
+        
+
+        System.out.printf("%-10s %-10s %-10s %-10s %-10s\n", "Product ID", "Name", "Price", "Status", "TEP");
+        Product pr = new Product();
+        pr.addprod(1011, "Soap", 20, 10, 200);
+        pr.viewprod();
+        
+        Product pr1 = new Product();
+        pr1.addprod(1012, "Cards", 50, 0, 0);
+        pr1.viewprod();
         
     }
     
