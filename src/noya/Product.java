c3c3e67b813d46c0640ee88b432f8affed78e818
NOYATHEGREAT <johnlloydisguapo@gmail.com>
2024-08-28 11:27:49 +0800

    Update Product.java

--- src/noya/Product.java
+++ src/noya/Product.java
@@ -1,14 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package noya;
 
-/**
- *
- * @author SCC-COLLEGE
- */
-public class Stockrecording {
+public class Product {
+    int id, price, stocks, tep;
+    String name;
     
+    
+    
+    public void addprod(int pid, String pname, int pr, int st, int teps){
+        
+        this.id = pid;
+        this.name = pname;
+        this.price = pr;
+        this.stocks = st;
+        this.tep = teps;
+        
+    }
+    
+    public void viewprod(){
+        double total = this.price * this.stocks;
+        String stats = (this.stocks == 0) ?"out of stocks" : "Available";
+        System.out.printf("%-10d %-10s %-10d %-10s %-20.2f\n", this.id, this.name,
+                this.price, stats, total);
+        
+    }
 }
