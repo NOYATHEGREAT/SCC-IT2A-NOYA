fcaa17513dc252b6ab5d0b589aee981afefd917b
NOYATHEGREAT <johnlloydisguapo@gmail.com>
2024-09-04 10:14:58 +0800

    Update Noya.java

--- src/noya/Noya.java
+++ src/noya/Noya.java
@@ -7,7 +7,7 @@ public class Noya {
         
         /* Grades grd = new Grades();
         grd.getGrades();
-        */
+        
         
 
         System.out.printf("%-10s %-10s %-10s %-10s %-10s\n", "Product ID", "Name", "Price", "Status", "TEP");
@@ -17,7 +17,35 @@ public class Noya {
         
         Product pr1 = new Product();
         pr1.addprod(1012, "Cards", 50, 0, 0);
-        pr1.viewprod();
+        pr1.viewprod(); 
+        
+        Product [] pr = new Product[100];
+        int i ;
+        System.out.println("Enter number of details : ");
+        int nprod = s.nextInt();
+        
+        for (i = 0; i < nprod; i++){
+            System.out.println("Enter no. of details: "+( i + 1 ) + ": ");
+            System.out.println("ID: ");
+            int id = s.nextInt();
+            System.out.println("Name: ");
+            int name = s.nextInt();
+            System.out.println("Price: ");
+            double p = s.nextDouble();
+            System.out.println("Stock: ");
+            int st = s.nextInt();
+            System.out.println("Sold: ");
+            int sold = s.nextInt();
+            pr[i] = new Product();
+            pr[i].addprod(id, name, p, s,sold);
+            
+            
+        }
+        
+        for (i = 0; i < nprod; i++)
+            pr[1].viewprod();*/
+         */
+        
         
     }
     
